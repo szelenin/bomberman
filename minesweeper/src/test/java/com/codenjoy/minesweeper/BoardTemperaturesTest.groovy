f3546c8726aae6b332d8f18735763b34ea927d07
szelenin <szelenin@playtika.com>
2015-04-04 23:39:40 +0300

    Hint calculation started

--- minesweeper/src/test/java/com/codenjoy/minesweeper/BoardTemperaturesTest.groovy
+++ minesweeper/src/test/java/com/codenjoy/minesweeper/BoardTemperaturesTest.groovy
@@ -7,17 +7,6 @@ import spock.lang.Unroll
  * Created by szelenin on 4/4/2015.
  */
 class BoardTemperaturesTest extends Specification {
-    def "tempearture when no hints"() {
-        when:
-        def temperatures = new BoardTemperatures(new Board("" +
-                "☼☼☼☼" +
-                "☼**☼" +
-                "☼**☼" +
-                "☼☼☼☼"))
-        then:
-        temperatures.temperatureAt(0, 0) == Double.MAX_VALUE
-        temperatures.temperatureAt(1, 1) == Double.MAX_VALUE - 1
-    }
 
     @Unroll
     def "tempearture when #coordinates"(String board, def coordinates, expectedTemperature) {
@@ -30,47 +19,47 @@ class BoardTemperaturesTest extends Specification {
 
         where:
         board        || coordinates                      || expectedTemperature
-//        '☼☼☼☼'       || [[0, 0], [1, 0], [0, 1], [1, 1]] || 100
-//        "" +
-//                "☼☼☼" +
-//                "☼*☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 1
-//        "" +
-//                "☼☼☼" +
-//                "☼1☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼2☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼3☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼4☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼5☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼6☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼7☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼8☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
-//        "" +
-//                "☼☼☼" +
-//                "☼ ☼" +
-//                "☼☼☼" || [[1, 1]] || 100 - 2
+        '☼☼☼☼'       || [[0, 0], [1, 0], [0, 1], [1, 1]] || 100
+        "" +
+                "☼☼☼" +
+                "☼*☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 1
+        "" +
+                "☼☼☼" +
+                "☼1☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼2☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼3☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼4☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼5☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼6☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼7☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼8☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
+        "" +
+                "☼☼☼" +
+                "☼ ☼" +
+                "☼☼☼" || [[1, 1]] || 100 - 2
         "" +
                 "☼☼☼☼☼" +
                 "☼***☼" +
@@ -78,24 +67,4 @@ class BoardTemperaturesTest extends Specification {
                 "☼***☼" +
                 "☼☼☼☼☼" || [[1, 1],[2,1],[3,1],[1,2],[3,2],[1,3],[2,3],[3,3]] || 1/8
     }
-/*
-    @Unroll
-    def "bomberman legal actions #expectedActions"(String board, List<Action> expectedActions) {
-        GameState state = new GameState(board)
-        expect:
-        (state.legalActions - expectedActions).empty
-        (expectedActions - state.legalActions).empty
-
-        where:
-        board                   || expectedActions
-        BOMBER_LEFT_UP_CORNER   || [RIGHT, DOWN, STOP, ACT]
-        BOMER_DOWN_RIGHT_CORNER || [UP, LEFT, STOP, ACT]
-        BOMB_BOMBER             || [UP, LEFT, STOP, ACT]
-        SURROUNDED_BY_OTHER     || [STOP, ACT]
-        SURROUNDED_BY_WALLS     || [LEFT, STOP, ACT]
-        SURROUNDED_BY_CHOPPERS  || [UP, LEFT, STOP, ACT]
-        SURROUNDED_BY_BOMBS     || [LEFT, RIGHT, UP, DOWN, STOP, ACT]
-        SURROUNDED_BY_EXPLOSION || [LEFT, RIGHT, UP, DOWN, STOP, ACT]
-    }
-*/
 }
