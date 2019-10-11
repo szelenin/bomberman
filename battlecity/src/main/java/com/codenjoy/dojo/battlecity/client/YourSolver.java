51ad8253a10b6d94de5e7eeba17ba1bda312c497
ansible-playbook -K ~/.ansible/ansible/ciab.yml <szelenin@salesforce.com>
2019-10-10 21:55:18 -0400

    Initial commit

---
+++ battlecity/src/main/java/com/codenjoy/dojo/battlecity/client/YourSolver.java
@@ -0,0 +1,60 @@
+package com.codenjoy.dojo.battlecity.client;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.codenjoy.dojo.services.Direction;
+import com.codenjoy.dojo.client.Solver;
+import com.codenjoy.dojo.client.WebSocketRunner;
+import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.RandomDice;
+
+/**
+ * User: your name
+ */
+public class YourSolver implements Solver<Board> {
+
+    private Dice dice;
+    private Board board;
+
+    public YourSolver(Dice dice) {
+        this.dice = dice;
+    }
+
+    @Override
+    public String get(Board board) {
+        this.board = board;
+        if (board.isGameOver()) return "";
+
+        return Direction.ACT();
+    }
+
+    public static void main(String[] args) {
+        WebSocketRunner.runClient(
+                // paste here board page url from browser after registration
+//                "http://codenjoy.com/codenjoy-contest/board/player/o2unia8kyovkg8eqgkk8?code=1341772214649572664",
+                "http://codenjoy.com/codenjoy-contest/board/player/hzmojevi35ng891j14a0?code=5949204546725679857",
+                new YourSolver(new RandomDice()),
+                new Board());
+    }
+
+}
